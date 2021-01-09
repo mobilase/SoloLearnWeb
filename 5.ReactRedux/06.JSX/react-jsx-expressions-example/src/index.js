9293f8e6831e8c61a8b0cd28a50c70f28636bcb2
Lavrov Sergey <lavrov-sergey@yandex.ru>
2021-01-09 20:56:07 +0300

    06.JSX

---
+++ 5.ReactRedux/06.JSX/react-jsx-expressions-example/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+const name = "David";
+const el = <p>Hello, {name}</p>;
+
+ReactDOM.render(
+  el,
+  document.getElementById('root')
+);

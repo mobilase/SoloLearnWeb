59383ecbcad819d0abadbf8b9a29f7ff0e740dcf
Lavrov Sergey <lavrov-sergey@yandex.ru>
2021-01-10 05:07:41 +0300

    20.Redux with React (change hooks to Redux)

--- 5.ReactRedux/20.Redux with React/react-hooks-counter-example/src/index.js
+++ 5.ReactRedux/20.Redux with React/react-hooks-counter-example/src/index.js
@@ -1,21 +1,63 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { connect } from 'react-redux';
 import './style.css';
 
-function Counter() {
-  const [counter, setCounter] = useState(0);
+// 1.Create action and reducer
+function incrementCounter(num) {
+  return {
+    type: 'INCREMENT',
+    num: num
+  }
+}
 
-  function increment() {
-    setCounter(counter+1);
+const initialState = {
+  count: 0
+};
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return { count: state.count + action.num };
+    default:
+      return state;
+  }
+}
+
+// 2.Create and connecting to the Store
+function mapStateToProps(state) {
+  return {
+    count: state.count
+  };
+}
+
+const mapDispatchToProps = {
+  incrementCounter
+}
+
+const store = createStore(reducer);
+
+// 3.Accessing The Store
+function Counter(props) {
+  function handleClick() {
+     props.incrementCounter(1);
   }
   return <div>
-  <p>{counter}</p>
-  <button onClick={increment}>Increment</button>
+  <p>{props.count}</p>
+  <button onClick={handleClick}>Increment</button>
   </div>;
 }
 
-const el = <Counter />; 
+const CounterConnect = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+const el =
+  <Provider store={store}>
+    <CounterConnect/>
+  </Provider>;
+
 ReactDOM.render(
-  el, 
+  el,
   document.getElementById('root')
 );
